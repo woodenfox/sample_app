9ec8742b3c1b4b3e7f1bf9717e9324834f9cb807
woodenfox <woodenfox@gmail.com>
2013-05-02 22:03:05 -0700

    Updating with Rsync & New Layouts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,51 +2,36 @@ require 'spec_helper'
 describe "Static pages" do
 
 	let(:base_title) {'UX Angels'}
+	subject {page}
 
 	describe "Home page" do
- 		it "should have the content 'Sample App'" do
- 			visit '/static_pages/home'
- 			page.should have_selector('h1', 
- 				:text =>'Sample App')
-    	end
-    	it "should have right title" do
-    		visit '/static_pages/home'
-    		page.should have_selector('title', 
-    			:text => 'UX Angels')
-   		end
+		before {visit root_path}
+		it { should have_selector('h1', text: 'Sample App')}
+    	it { should have_selector('title', :text => 'UX Angels')}
+   		it { should have_selector('footer')}
+   		it { should have_selector('header')}
 	end
+
 	describe "Help Page" do
-		it "should have the text 'Help' visibile" do
-			visit '/static_pages/help'
-			page.should have_selector('h1',
-				:text => 'Help')
-		end
-		it "should have right title" do
-			visit '/static_pages/help'
-			page.should have_selector('title', 
-				:text => 'UX Angels | Help')
-		end
+		before {visit help_path}
+		it { should have_selector('h1', text: 'Help')}
+		it { should have_selector('title', text: 'UX Angels | Help')}
 	end   
+
 	describe "About Page" do
-		it "should have the text 'About Us' visibile" do
-			visit '/static_pages/about'
-			page.should have_selector('h1',
-				:text => 'About Us')
-		end
-		it "should have right title" do
-			visit '/static_pages/about'
-			page.should have_selector('title', 
-				:text => 'UX Angels | About Us')
-		end
+		before {visit about_path}
+		it {should have_selector('h1', text: 'About Us')}
+		it {should have_selector('title', text: 'UX Angels | About Us')}
 	end
+
 	describe "Contact Us" do
 		it "should have the text 'Contact Us' visibile" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('h1',
 				:text => 'Contact Us')
 		end
 		it "should have right title" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('title', 
 				:text => 'UX Angels | Contact Us')
 		end
